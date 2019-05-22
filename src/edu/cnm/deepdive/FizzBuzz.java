16a15ca65281eb4df0956ec3f8901adc542f1e8d
matthewjones <matthewjones2435@gmail.com>
2019-05-22 14:25:19 -0600

    Added javadocs comments & generated HTML documentation.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -1,10 +1,29 @@
 package edu.cnm.deepdive;
 
+/**
+ * Includes several public methods and constants for computing FizzBuzz values.
+ * <p> This class includes <code>static</code> methods that take <code>int</code> parameters and
+ * return <code>boolean</code> and {@link String} results, indicating whether the specified
+ * values are divisible by 3 (fizz) or 5 (buzz).</p>
+ *
+ * @author Matthew Jones &amp; Deep Dive Coding Java+Android Cohort 7.
+ * @version 1.0.0
+ */
 public class FizzBuzz {
 
+  /** {@link String} value indicating a number divisible by 3. */
   public static final String FIZZ = "Fizz";
+  /** {@link String} value indicating a number divisible by 5. */
   public static final String BUZZ = "Buzz";
 
+  private FizzBuzz() {}
+  /**
+   * Computes and returns "Fizz", "Buzz", or a {@link String} representation of <code>value</code>,
+   * depending on whether <code>value</code> is divisble by 3 or 5.
+   *
+   * @param value integer value to be tested for divisibility by 3 or 5.
+   * @return "fizz", "Buzz", or value as a String.
+   */
   public static String fizzBuzzValue(int value) {
     if (value <= 0) {
       throw new IllegalArgumentException();
@@ -22,10 +41,20 @@ public class FizzBuzz {
     return result;
   }
 
+  /**
+   * Computes and returns true if <code>value</code> is divisible by 3, otherwise false.
+   * @param value integer <code>value</code> to be tested for divisibility by 3
+   * @return rue or false depending on whether <code>value</code> is divisible by 3.
+   */
   public static boolean isFizz(int value) {
     return value % 3 == 0;
   }
 
+  /**
+   * Computes and returns true if <code>value</code> is divisible by 5, otherwise false.
+   * @param value integer <code>value</code> to be tested for divisibility by 5.
+   * @return true or false depending on whether <code>value</code> is divisible by 5.
+   */
   public static boolean isBuzz(int value) {
     return value % 5 == 0;
   }
--- src/edu/cnm/deepdive/Main.java
+++ src/edu/cnm/deepdive/Main.java
@@ -1,7 +1,15 @@
 package edu.cnm.deepdive;
 
+/**
+ * Main method that runs methods of and prints results from the {@link FizzBuzz} class.
+ */
 public class Main {
 
+  private Main() {}
+  /**
+   * Computes and displays values as fizz or buzz corresponding to their input values 1&ndash;100
+   * @param args Command line arguments
+   */
   public static void main(String[] args) {
     for(int i = 1; i <= 100; i++) {
       System.out.println (FizzBuzz.fizzBuzzValue(i));
